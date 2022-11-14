18c2d691d51ac80b41e0f615c84b26e1399884f3
Daniel Avery <danielaveryj@gmail.com>
2022-11-14 11:57:21 -0700

    Finish high-level documentation (README and package doc).

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Vinyl: Relational Record Streams
 
+Vinyl is now in beta. Feedback is welcome!
+
 Inspired by SQL, built for Java. Vinyl takes the expressive power of SQL queries and reimagines it on top of Java
-Streams. Through carefully designed abstractions, relational operations prioritize safety without compromising
-terseness. Seamless integration with Java Streams achieves efficiency and parallelism, while enabling new extremes of
-flexibility.
+Streams. By leaning into existing JDK abstractions, Vinyl achieves seamless integration with Java Streams, leveraging
+their efficiency and parallelism while introducing new avenues of flexibility. A carefully controlled API enables
+relational operations to prioritize safety without sacrificing brevity.
+
+For more information and examples, see the package documentation.
--- src/main/java/vinyl/package-info.java
+++ src/main/java/vinyl/package-info.java
@@ -23,11 +23,90 @@
  */
 
 /**
- * Vinyl is a library for streaming relational records.
+ * Classes to support relational-style operations on streams of elements, such as select, aggregate, and join
+ * transformations. The elements are represented as immutable "record" objects, which have their "fields" defined by a
+ * relational operation, rather than a class declaration. For example:
  *
- * <p>Vinyl offers SQL-inspired operations on its "record-streams"
+ * <pre>{@code
+ *     Field<Long> userAge = new Field<>("userAge");
+ *     Field<Long> userAccountAge = new Field<>("userAccountAge");
+ *     Field<Long> countUsers = new Field<>("countUsers");
+ *     Field<Double> avgAccountAge = new Field<>("avgAccountAge");
  *
- * <p>The design of Vinyl is influenced primarily by a strong desire to achieve <b>mechanical sympathy</b> with the
- * "Java way". It is inspired by OpenJDK implementation patterns, interfaces, and intent.
+ *     RecordSet data = RecordStream.aux(users.stream())
+ *         .mapToRecord(into -> into
+ *             .field(userAge, user -> ChronoUnit.YEARS.between(user.getBirthDate(), LocalDate.now()))
+ *             .field(userAccountAge, user -> ChronoUnit.YEARS.between(user.getAccountStartDate(), LocalDate.now()))
+ *         )
+ *         .aggregate(aggregate -> aggregate
+ *             .keyField(userAge)
+ *             .aggField(countUsers, Collectors.counting())
+ *             .aggField(avgAccountAge, Collectors.averagingLong(record -> record.get(userAccountAge))
+ *         )
+ *         .toRecordSet();
+ * }</pre>
+ *
+ * <p>Here we first define a few fields to use in subsequent relational operations, giving types for those fields. Then we
+ * take a stream of {@code users}, transform it to a stream of records, and perform a grouped aggregation yielding a new
+ * stream of records, which we finally collect into a {@code RecordSet}.
+ *
+ * <h2><a id="Records">Records, Headers, and Fields</a></h2>
+ *
+ * <p>{@code Record} is an essential abstraction in Vinyl. A record is a shallowly-immutable carrier for a fixed set of
+ * values, not unlike a {@code java.lang.Record} in Java 16+. However, Vinyl Records are not open to extension, and do
+ * not declare any fields on the class. Instead, records are internally instantiated with a {@code Header} and a set of
+ * values. The header defines what "fields" the record contains, and the values correspond to those fields. This allows
+ * relational operations to dynamically define new combinations of fields for resulting records.
+ *
+ * <p>Each "field" in the record header is represented as a {@code Field}. A field is an opaque object with a
+ * parameterized type - the type of values that a record may associate with that field. The field is used to access its
+ * value on a record, similar to how normal fields are used to access values on an object. This ensures value access is
+ * type-safe.
+ *
+ * <h2><a id="Aux">Auxiliary Streams</a></h2>
+ *
+ * <p>In the above "user aggregate" example, we first had to wrap the default {@code Stream} of users in a
+ * {@code RecordStream.Aux}. This wrapper-stream extends the default stream with the {@code mapToRecord()} method, which
+ * enables transformation into a {@code RecordStream}. Rather than have a static method to do this, using a
+ * wrapper-stream allows for going back and forth between the stream types seamlessly. Other wrapper-stream types are
+ * provided for each of the primitive stream types, ie {@code RecordStream.AuxInt}, {@code RecordStream.AuxLong}, and
+ * {@code RecordStream.AuxDouble}. Existing stream operations that return a stream are overridden to return one of these
+ * wrapper types.
+ *
+ * <h2><a id="RecordStreams">RecordStreams and RecordSets</a></h2>
+ *
+ * <p>A {@code RecordStream} is a stream of records sharing the same header. Record-streams extend normal streams with
+ * relational operations like {@code select()}, {@code aggregate()}, and {@code join()}, which transform the header and
+ * records in the stream.
+ *
+ * <p>A {@code RecordSet} stores records sharing the same header. Record-streams can be collected into record-sets,
+ * allowing the records to be trivially re-streamed later, possibly multiple times.
+ *
+ * <h2><a id="Configurators">Configurators</a></h2>
+ *
+ * <p>Relational operations on a record-stream expose "configurators", which configure how the operation is executed. For
+ * example, configurators are used to define the output fields of a {@code select()} operation, the join condition of a
+ * {@code join()} operation, and the grouping keys of an {@code aggregate()} operation. Configurators may expose
+ * sub-configurators for handling isolated specifics of the overall operation. To demonstrate:
+ *
+ * <pre>{@code
+ *     scoresStream
+ *         .select(select -> select
+ *             .field(points)
+ *             .window(window -> window
+ *                 .field(averagePoints, Analytics.fromAgg(Collectors.averagingLong(points::get)))
+ *                 .fields(Comparator.comparingLong(points::get), fields -> fields
+ *                     .field(discreteMedian, Analytics.percentileDisc(0.5, points::get))
+ *                     .field(continuousMedian, Analytics.percentileCont(0.5, points::get))
+ *                 )
+ *             )
+ *         )
+ * }</pre>
+ *
+ * <p>Here, the {@code select}, {@code window}, and {@code fields} configurators (named after the methods that introduce
+ * them) are used to configure the overall {@code select()} operation. The {@code window} configurator is a
+ * sub-configurator of {@code select}, and the {@code fields} configurator is a sub-configurator of {@code window}.
+ * Together these configurators define four fields {@code [points, averagePoints, discreteMedian, continuousMedian]} on
+ * the resultant record-stream.
  */
 package vinyl;
